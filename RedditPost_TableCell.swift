7dc780269478a0e19869540cedaa34facc904b14
damian930 <damiandragovoz@gmail.com>
2025-04-08 13:13:01 +0300

    Just removed some comments

--- RedditPost_TableCell.swift
+++ RedditPost_TableCell.swift
@@ -12,7 +12,7 @@ final class RedditPost_TableCell: UITableViewCell {
     @IBOutlet weak var redditPostView: RedditPostView!
     
 //    TODO: maybe use a trait, something like cell tappable and cell double tappable
-    /*private*/ weak var vc: RedditPost_SingleTappable?
+    private weak var vc: RedditPost_SingleTappable?
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -37,7 +37,7 @@ final class RedditPost_TableCell: UITableViewCell {
         self.addGestureRecognizer(singleTapGesture)
         
         // TODO: remove the double tap from here, but cant do it now, since it throws an error in one of the views. Dont know why, those 2 have nothing to do with 1 another
-        // Now i know, it throws it, cause 1 outler is not initilaised before used, its not that big of a deal, but th fact that this when removed result in that just doesnt make sense at all
+        // Now i know, it throws it, cause 1 outler is not initilaised before used, its not that big of a deal, but the fact that this when removed results in a runtime error just doesnt make sense at all
         let doubleTapGesture = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap))
         doubleTapGesture.numberOfTapsRequired = 2
         self.addGestureRecognizer(doubleTapGesture)
@@ -54,7 +54,7 @@ final class RedditPost_TableCell: UITableViewCell {
         guard let post = self.redditPostView.post else {
             assert(false, "Trying to use a nil redditPost, not allowed")
         }
-        print("1")
+        // print("1")
         vc.singleTapHandler(post: post)
 
 
